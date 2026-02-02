75c3fbb7c2935aa4fe785352d80452f3e3178519
Ruhan12082006 <ruhanghantiwala@gmail.com>
2026-02-02 14:51:26 +0200

    made the table

--- src/ex1.php
+++ src/ex1.php
@@ -1,9 +1,35 @@
 <html>
 <body>
+
     <?php
     echo "<h3> Hello world! My name is \"David\" </h3>";
     $title ="PHP is interesting.";
     echo "<h4> $title </h4>";
+    $g1 = 5;
+    $g2 = 4;
+    $g3 = 5;
+    echo "<table>
+        <tr>
+            <td>S.n.</td>
+            <td>Name</td>
+            <td>Grade</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>John</td>
+            <td> $g1 </td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Alice</td>
+            <td> $g2 </td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Alice</td>
+            <td> $g3 </td>
+        </tr>
+    </table>";
     ?>
 </body>
 </html>
